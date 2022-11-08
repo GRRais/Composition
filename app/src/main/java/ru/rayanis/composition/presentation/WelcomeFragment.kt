d8ca2aa049cf50b016a08c1797e69ca8ea0dffdf
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-08 20:34:27 +0300

    lesson 6.42 8.48

--- app/src/main/java/ru/rayanis/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/ru/rayanis/composition/presentation/WelcomeFragment.kt
@@ -1,19 +1,33 @@
 package ru.rayanis.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.rayanis.composition.R
+import ru.rayanis.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
 
+    private var _b: FragmentWelcomeBinding? = null
+    private val b: FragmentWelcomeBinding
+    get() = _b ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome , container , false)
+    ): View {
+        _b = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return b.root
+    }
+
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _b = null
     }
 }
