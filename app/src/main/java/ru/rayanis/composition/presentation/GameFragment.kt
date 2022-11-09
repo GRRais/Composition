f6a487a6e6cde4995622879d193a73dedb910451
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-09 05:53:19 +0300

    lesson 7.43 3.38

--- app/src/main/java/ru/rayanis/composition/presentation/GameFragment.kt
+++ app/src/main/java/ru/rayanis/composition/presentation/GameFragment.kt
@@ -6,13 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.rayanis.composition.R
+import ru.rayanis.composition.databinding.FragmentChooseLevelBinding
+import ru.rayanis.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _b: FragmentGameBinding? = null
+    private val b: FragmentGameBinding
+        get() = _b ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game , container , false)
+    ): View {
+        _b = FragmentGameBinding.inflate(inflater , container , false)
+        return b.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _b = null
     }
 }
