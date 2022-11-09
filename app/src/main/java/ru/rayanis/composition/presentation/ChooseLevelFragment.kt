a6693db155578e9075d3d1a787534bafe8396292
Rais Garifzyanov <garifzyanov.rr@gmail.com>
2022-11-09 16:29:49 +0300

    7.43 10,50

--- app/src/main/java/ru/rayanis/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/ru/rayanis/composition/presentation/ChooseLevelFragment.kt
@@ -7,20 +7,46 @@ import android.view.View
 import android.view.ViewGroup
 import ru.rayanis.composition.R
 import ru.rayanis.composition.databinding.FragmentChooseLevelBinding
+import ru.rayanis.composition.domain.entity.Level
 
 
 class ChooseLevelFragment : Fragment() {
 
     private var _b: FragmentChooseLevelBinding? = null
     private val b: FragmentChooseLevelBinding
-    get() = _b ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+        get() = _b ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
     override fun onCreateView(
-        inflater: LayoutInflater , container: ViewGroup? ,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _b = FragmentChooseLevelBinding.inflate(inflater , container , false)
+        _b = FragmentChooseLevelBinding.inflate(inflater, container, false)
         return b.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(b) {
+            buttonLevelTest.setOnClickListener {
+                launchGameFragment(Level.TEST)
+            }
+            buttonLevelEasy.setOnClickListener {
+                launchGameFragment(Level.EASY)
+            }
+            buttonLevelNormal.setOnClickListener {
+                launchGameFragment(Level.NORMAL)
+            }
+            buttonLevelHard.setOnClickListener {
+                launchGameFragment(Level.HARD)
+            }
+        }
+    }
+
+    private fun launchGameFragment(level: Level) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onDestroy() {
@@ -32,6 +58,5 @@ class ChooseLevelFragment : Fragment() {
         fun newInstance(): ChooseLevelFragment {
             return ChooseLevelFragment()
         }
-
     }
 }
--- app/src/main/java/ru/rayanis/composition/presentation/GameFragment.kt
+++ app/src/main/java/ru/rayanis/composition/presentation/GameFragment.kt
@@ -8,12 +8,20 @@ import android.view.ViewGroup
 import ru.rayanis.composition.R
 import ru.rayanis.composition.databinding.FragmentChooseLevelBinding
 import ru.rayanis.composition.databinding.FragmentGameBinding
+import ru.rayanis.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private lateinit var level: Level
 
     private var _b: FragmentGameBinding? = null
     private val b: FragmentGameBinding
         get() = _b ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
@@ -26,5 +34,22 @@ class GameFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _b = null
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object {
+
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
